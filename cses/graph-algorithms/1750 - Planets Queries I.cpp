cc342dbdb7cdb6bf610abd9bcc5e62698978b0f9
ANURAG KANOGIA <166480337+Anuragkanogia@users.noreply.github.com>
2026-08-26 17:54:50 +0530

    Add Planets Queries I (graph-algorithms)

---
+++ cses/graph-algorithms/1750 - Planets Queries I.cpp
@@ -0,0 +1,74 @@
+// Author: Anurag 
+// 
+// Problem: Planets Queries I
+// Contest: CSES - CSES Problem Set
+// URL: https://cses.fi/problemset/task/1750
+// Memory Limit: 512 MB
+// Time Limit: 1000 ms
+
+#include <bits/stdc++.h>
+#define ll long long
+#define int long long 
+//#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+using namespace std;
+using vpi = vector<pair<int, int>>;
+using pi = pair<int, int>;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+#define ff first
+#define ss second
+#define all(x) x.begin(), x.end()
+//#define sz(x) (int)(x).size()
+const int mod = 1e9 + 7;
+#define MOD (1000000007);
+const int NUM = 1000030;
+const int N = 1e7 + 10;
+#define DEBUG(x) cerr << #x << ": " << x << '\n'
+const int MAXN = 200005;
+const int LOG = 32; // 2^30 > 10^9
+
+int up[LOG][MAXN];
+int getkthancestor(int u, int k) {
+    for (int i = 0; i < LOG; i++) {
+        if (k & (1LL << i)) {
+            u = up[i][u]; // FIXED: up[power][node]
+        }
+    }
+    return u;
+}
+void solve(){
+ 
+int n,q;
+cin>>n>>q;
+
+for(int i = 1;i<= n;i++){
+	int a;
+	cin>>a;
+	up[0][i] = a;
+} 
+for(int i = 1;i<=31;i++){
+ for(int u = 1;u<= n;u++){
+ 		up[i][u] = up[i-1][up[i-1][u]];
+ 	}
+ }
+ while(q--){
+ 	int u , k;
+ 	cin>>u>>k;
+ 	cout<<getkthancestor(u, k)<<'\n';
+ }
+    
+   }
+int32_t main()
+{
+    ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+ //sieve();
+  
+ 
+solve();
+
+}
