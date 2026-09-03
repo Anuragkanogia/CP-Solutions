4898cf593e964f148f6a87c7efc89b8f92672f24
ANURAG KANOGIA <166480337+Anuragkanogia@users.noreply.github.com>
2026-09-03 18:29:07 +0530

    Add Deletion Sort (800)

---
+++ codeforces/800/2200B - Deletion Sort.cpp
@@ -0,0 +1,55 @@
+// Problem: B. Deletion Sort
+// Contest: Codeforces - Codeforces Round 1084 (Div. 3)
+// URL: https://codeforces.com/problemset/problem/2200/B
+// Memory Limit: 256 MB
+// Time Limit: 1000 ms
+// 
+// Powered by CP Editor (https://cpeditor.org)
+
+#include <bits/stdc++.h>
+#define ll long long
+#define int long long 
+//#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+using namespace std;
+using vpi = vector<pair<int, int>>;
+using pi = pair<int, int>;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+#define ff first
+#define ss second
+#define all(x) x.begin(), x.end()
+//#define sz(x) (int)(x).size()
+const int mod = 1e9 + 7;
+#define MOD (1000000007);
+const int NUM = 1000030;
+const int N = 1e7 + 10;
+#define DEBUG(x) cerr << #x << ": " << x << '\n'
+void solve(){
+ 
+ int n;
+ cin>>n;
+ vector<int> a(n);
+ for(int i = 0;i< n;i++){
+ 	cin>>a[i];
+ }
+ if(is_sorted(all(a)) == true)cout<<n<<endl;
+ else cout<<1<<endl;
+ 
+    
+   }
+int32_t main()
+{
+    ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+ //sieve();
+  
+int t;
+   cin>>t;
+ while(t--){  
+solve();
+}
+}
