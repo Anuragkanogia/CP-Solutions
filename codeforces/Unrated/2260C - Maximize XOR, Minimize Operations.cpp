96441e825284ce213f7b5dc6aed28cc4e330adce
ANURAG KANOGIA <166480337+Anuragkanogia@users.noreply.github.com>
2026-09-09 19:55:17 +0530

    Add Maximize XOR, Minimize Operations (Unrated)

---
+++ codeforces/Unrated/2260C - Maximize XOR, Minimize Operations.cpp
@@ -0,0 +1,62 @@
+// Problem: C. Maximize XOR, Minimize Operations
+// Contest: Codeforces - Educational Codeforces Round 194 (Rated for Div. 2)
+// URL: https://codeforces.com/contest/2260/problem/C
+// Memory Limit: 512 MB
+// Time Limit: 2000 ms
+// 
+// Powered by CP Editor (https://cpeditor.org)
+
+#include <bits/stdc++.h>
+#define ll long long
+#define int long long 
+//#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+using namespace std;
+using vpi = vector<pair<int, int>>;
+using pi = pair<int, int>;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+#define ff first
+#define ss second
+#define all(x) x.begin(), x.end()
+//#define sz(x) (int)(x).size()
+const int mod = 1e9 + 7;
+#define MOD (1000000007);
+const int NUM = 1000030;
+const int N = 1e7 + 10;
+#define DEBUG(x) cerr << #x << ": " << x << '\n'
+void solve(){
+
+int x,y;
+cin>>x>>y;
+int s= x+ y;
+int temp = 0;
+int cnt = 0;
+for (int i = 31; i >= 0; i--) {
+
+    if (s & (1LL << i)) {
+
+        if (temp + (1LL << i) <= x) {
+            temp |= (1LL << i);
+            cnt++;
+        }
+    }
+}
+cout<<s <<" "<<x - temp<<endl;
+}
+   
+int32_t main()
+{
+    ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+ //sieve();
+  
+int t;
+   cin>>t;
+ while(t--){  
+solve();
+}
+}
