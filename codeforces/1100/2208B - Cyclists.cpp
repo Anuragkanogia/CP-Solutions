8a292d6f0a4d5a4b3c03f6cb51d8de9ab8902dec
ANURAG KANOGIA <166480337+Anuragkanogia@users.noreply.github.com>
2026-09-11 12:09:20 +0530

    Add Cyclists (1100)

--- codeforces/1100/2208B - Cyclists.cpp
+++ codeforces/1100/2208B - Cyclists.cpp
@@ -28,48 +28,47 @@ const int N = 1e7 + 10;
 #define DEBUG(x) cerr << #x << ": " << x << '\n'
 void solve(){
  
-int n, k, p, m;
-    cin >> n >> k >> p >> m;
-
-    vector<int> a(n);
-    for (int i = 0; i < n; i++) cin >> a[i];
-
-    int val = a[p - 1];
-    a[p - 1] = -1;
-
-    priority_queue<int, vector<int>, greater<int>> pq;
-    queue<int> q;
-
-    for (int i = 0; i < k; i++) pq.push(a[i]);
-    for (int i = k; i < n; i++) q.push(a[i]);
-
-    int cnt = 0;
-
-    while (!pq.empty()) {
-        int x = pq.top();
-
-        // Check required energy BEFORE playing the card
-        int cost = (x == -1) ? val : x;
-        if (m < cost) break;
-
-        pq.pop();
-        m -= cost;
-
-        if (x == -1) {
-            cnt++;
-        }
-
-        // Cycle the played card to the back
-        q.push(x);
-
-        // Bring the next queue element into the priority queue
-        if (!q.empty()) {
-            pq.push(q.front());
-            q.pop();
-        }
-    }
-
-    cout << cnt << "\n";
+ int n, k , p , m;
+ cin>>n>>k>>p>>m;
+ vector<int> a(n);
+ for(int i = 0;i< n;i++){
+ 	cin>>a[i];
+ }
+ int val = a[p-1];
+ a[p-1]  = -1;
+ 
+ priority_queue<int, vector<int> , greater<int>> pq;
+ queue<int> q;
+ for(int i = 0;i<k;i++){
+ 	pq.push(a[i]);
+ }
+ for(int i = k;i< n;i++){
+ 	q.push(a[i]);
+ }
+ int cnt =0;
+  while(pq.empty() == false){
+  	int x= pq.top();
+  	pq.pop();
+  	
+  	if(x == -1){
+  		if(m < val)break;
+  		cnt++;
+  	
+  	m-= val;
+  	}
+  	else {
+  		if(m< x)break;
+  		m-= x;
+  	}
+  	q.push(x);
+  	if(q.empty() == false){
+  	int y = q.front();
+  		q.pop();
+  	pq.push(y);
+  	}
+  
+  }  
+  cout<<cnt<<endl;
    }
 int32_t main()
 {
