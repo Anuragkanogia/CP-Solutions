fca97ce2220eebdb05398a2e256fbd500a7bb550
ANURAG KANOGIA <166480337+Anuragkanogia@users.noreply.github.com>
2026-09-25 07:24:00 +0530

    Add Palindrome-Number (math)

---
+++ leetcode/math/9-Palindrome-Number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+      string s= to_string(x);
+      int i = 0;
+      while(i<s.size()){
+        if(s[i] != s[s.size() - i-1]){
+            return false;
+        }
+        i++;
+      }
+      return true;
+    }
+};
